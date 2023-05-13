1eb52000e76c49803d2df253c24876a002bff3ce
AV-G <68665243+AV-G@users.noreply.github.com>
2023-05-13 23:27:53 +0200

    Graph library

--- src/components/Page.Setup.tsx
+++ src/components/Page.Setup.tsx
@@ -2,7 +2,7 @@ import { IconButton } from "@mui/material";
 import {Settings, MonitorHeartOutlined, NearMe, MicRounded} from "@mui/icons-material";
 import React from "react";
 import LoginButton from "./LoginButton";
-
+import Graph from "./Graph";
 function SetPage(){
     return (
         <div className="page">
