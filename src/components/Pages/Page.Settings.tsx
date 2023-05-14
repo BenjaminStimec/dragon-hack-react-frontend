a3603ec433ffd11e32ed2cd588c417838dfbc1b2
Klemen <50655512+Klemen2@users.noreply.github.com>
2023-05-14 07:20:31 +0200

    merge fix

--- src/components/Pages/Page.Settings.tsx
+++ src/components/Pages/Page.Settings.tsx
@@ -1,12 +1,10 @@
 import { IconButton } from "@mui/material";
 import {Settings, MonitorHeartOutlined, NearMe, MicRounded} from "@mui/icons-material";
 import React from "react";
-import LogoutButton from "../LogoutButton";
 
 function SettingsPage(){
     return (
         <div className="page">
-        <LogoutButton/>
         </div>
     )
 };
