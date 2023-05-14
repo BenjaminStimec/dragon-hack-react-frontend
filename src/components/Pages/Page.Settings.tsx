307119f4177427eefbda1b148d084ecace194d67
AV-G <68665243+AV-G@users.noreply.github.com>
2023-05-14 09:28:34 +0200

    Page Settings

--- src/components/Pages/Page.Settings.tsx
+++ src/components/Pages/Page.Settings.tsx
@@ -1,10 +1,45 @@
 import { IconButton } from "@mui/material";
 import {Settings, MonitorHeartOutlined, NearMe, MicRounded} from "@mui/icons-material";
 import React from "react";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
 
 function SettingsPage(){
     return (
         <div className="page">
+            <List>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                    <ListItemText primary="About" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                    <ListItemText primary="Credits" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                    <ListItemText primary="Setup" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                    <ListItemText primary="My Data" />
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton>
+                    <ListItemText primary="Reset Settings" />
+                    </ListItemButton>
+                </ListItem>
+        </List>
         </div>
     )
 };
